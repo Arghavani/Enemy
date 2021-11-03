46cf4d9172bda2bea227aa43d7387e6ac4e77e27
Arghavani <robabehizad25@gmail.com>
2021-11-03 21:32:17 +0100

    Add I want to suck their blood message.

--- src/Vampire.java
+++ src/Vampire.java
@@ -2,5 +2,7 @@ public class Vampire extends Enemy{
 
     public void Talk(){
         System.out.println("I want to suck your blood");
+        System.out.println("I want to suck their blood");
+
     }
 }
