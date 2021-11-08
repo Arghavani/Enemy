f0656d55259ce18d5a64ea4cd52f9b8dc0181ad8
Arghavani <robabehizad25@gmail.com>
2021-11-08 18:18:05 +0100

    bugfix

--- src/Vampire.java
+++ src/Vampire.java
@@ -3,6 +3,7 @@ public class Vampire extends Enemy{
     public void Talk(){
         System.out.println("I want to suck your blood");
         System.out.println("I want to suck their blood");
+        System.out.println("this is a  bugfix");
 
     }
 }
